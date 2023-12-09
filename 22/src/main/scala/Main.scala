e04e4dde9e042fca1ea86ba96504d558b5ade1da
Egor <etkachev398@gmail.com>
2023-12-09 23:05:01 +0300

    Added task 22

---
+++ 22/src/main/scala/Main.scala
@@ -0,0 +1,32 @@
+import java.io.FileInputStream
+  import opennlp.tools.namefind.{NameFinderME, TokenNameFinderModel}
+  import opennlp.tools.tokenize._
+  import opennlp.tools.util.Span
+  
+  object Main {
+      def main(args:Array[String]): Unit = {
+          val tokenizerModelIn = new FileInputStream("en-token.bin")
+          val tokenizerModel = new opennlp.tools.tokenize.TokenizerModel(tokenizerModelIn)
+          val tokenizer = new opennlp.tools.tokenize.TokenizerME(tokenizerModel)
+          // Load NER model
+          val nerModelIn = new FileInputStream("en-ner-person.bin")
+          val nerModel = new TokenNameFinderModel(nerModelIn)
+          val ner = new NameFinderME(nerModel)
+  
+    		// Define some sample text
+          val text = "John Smith is a software engineer at Google."
+  
+          // Tokenize the text
+          val tokens = tokenizer.tokenize(text)
+  
+    		// Find the named entities in the text
+          val spans = ner.find(tokens)
+  
+          // Print the named entities and their types
+          for(span <- spans) {
+              val entityType = span.getType
+              val entity = tokens.slice(span.getStart, span.getEnd).mkString(" ")
+              println(s"$entityType: $entity")
+          }
+      }
+  }
